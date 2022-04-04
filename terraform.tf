86e1ddb7dcf250160f36f3ed6e4ebdab84aea2f9
tea-short <dmitrijs.kovalenko@gmail.com>
2022-04-04 13:04:11 +0100

    fmt

--- terraform.tf
+++ terraform.tf
@@ -20,7 +20,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  region     = "eu-west-2"
   access_key = "AKIAIOSFODNN7EXAMPLE"
   secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 }
