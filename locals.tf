2c1b009cabc7e79523c903c1b18ec8c5dca08002
tea-short <dmitrijs.kovalenko@gmail.com>
2022-04-04 12:37:41 +0100

    fmt

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
   application = "tf-gha"
   env         = terraform.workspace == "default" ? "dev" : terraform.workspace
 
-  region  = data.aws_region.current.name
+  region = data.aws_region.current.name
 
   gha_user_name = "gha"
 
